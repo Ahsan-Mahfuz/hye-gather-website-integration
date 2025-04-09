1d1abac823693bbde617c2d6527343fad402b7ea
ahsan-mahfuz <ahsanmahfuz0@gmail.com>
2025-04-09 07:33:14 +0600

    redirect page

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['randomuser.me', '10.0.60.23', '10.0.60.189', 'dummyimage.com/'],
+    domains: ['randomuser.me', '10.0.60.23', '10.0.60.189', 'dummyimage.com'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
   },
 }
 
